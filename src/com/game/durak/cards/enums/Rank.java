489dedfb5ceb6e55c1420fce4e3e86e47383611b
Ilyas Makirov <ilmak1704@gmail.com>
2024-11-16 01:19:59 +0500

    added some logic

--- src/com/game/durak/cards/enums/Rank.java
+++ src/com/game/durak/cards/enums/Rank.java
@@ -1,5 +1,5 @@
 package com.game.durak.cards.enums;
 
 public enum Rank {
-    SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACEм=ильяс лоs
+    SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 }
